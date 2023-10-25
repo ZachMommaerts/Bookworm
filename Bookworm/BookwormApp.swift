d9cd7ddf3283de3afe304bbe60b74c0039470d3d
ZachMommaerts <zmommaerts@gmail.com>
2023-10-24 23:44:53 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Zach Mommaerts on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
